aa62ab4314752984f9b3b479be22cb94a1a0f9f9
slthurman01 <slthurman01@gmail.com>
2023-01-31 13:28:20 -0600

    Adds jest and fixes test bugs.

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -3,13 +3,15 @@ const Intern = require("../lib/Intern");
 describe("Intern", () => {
 
     describe("Creation", () => {
-        it("should create an object with a name, id, email, and school", () => {
+        it("should create an object with a name, id, email, and school. Should return employee position when getting role", () => {
             const employee = new Intern("Laura", 1, "laurales@gecs.gov", "GEC");
 
             expect(employee.name).toEqual("Laura");
             expect(employee.id).toEqual(1);
             expect(employee.email).toEqual("laurales@gecs.gov");
             expect(employee.school).toEqual("GEC");
+
+            expect(employee.getRole()).toEqual("Intern");
         });
     });
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,13 +3,15 @@ const Manager = require("../lib/Manager");
 describe("Manager", () => {
 
     describe("Creation", () => {
-        it("should create an object with a name, id, email, and office", () => {
-            const employee = new Intern("Gaga", 1, "ladygaga@badromance.org", "143");
+        it("should create an object with a name, id, email, and office. Should return employee position when getting role", () => {
+            const employee = new Manager("Gaga", 1, "ladygaga@badromance.org", "143");
 
             expect(employee.name).toEqual("Gaga");
             expect(employee.id).toEqual(1);
             expect(employee.email).toEqual("ladygaga@badromance.org");
-            expect(employee.office).toEqual("143");
+            expect(employee.officeNumber).toEqual("143");
+
+            expect(employee.getRole()).toEqual("Manager");
         });
     });
 });
